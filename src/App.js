6794b976a3b12c4adf717b5b77d5aeca59ac1628
Anuj Kumar Kheria <Anuj.Kheria@insightdelivered.com>
2024-06-17 15:04:48 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
-import data from './data'
-import List from './List'
+// imports
+//use state - for state management - for example changeing a value on the screen in case of an event like onclick
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  const [people, setpeople] = useState(data)
+  //variable declaration
+  //let - variable that can change
+  //var - this old and depriciated.
+  //const - this is always constant
+
+  // useState syntax
+  // people - variable setpeople - function
+  //const [variable, <- function] = useState(initial value)
+  const [people, setpeople] = useState(data);
+  console.log(people);
   return (
     <main>
-      <section className='container'>
+      <section className="container">
         <h3>{people.length} birthday's today</h3>
         <List people={people} />
+        {/* {people} */}
         <button onClick={() => setpeople([])}>clear all</button>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
